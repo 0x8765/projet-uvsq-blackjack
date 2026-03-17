4c1020d3bd7fa39e1e5e75613cde0edc2abd12b2
victor <soudeur-epicier.0x@icloud.com>
2026-03-17 10:46:09 +0100

    ajout de maceo

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,25 @@
 import random
+
+
+def creer_paquet():
+    """
+    Ne prend aucun paramètre
+    Retourne un paquet de 52 cartes sous forme de liste de tuples
+    """
+    valeurs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
+    couleurs = ['Cœur', 'Carreau', 'Trèfle', 'Pique']
+
+    paquet = [] 
+    for couleur in couleurs: 
+        for valeur in valeurs: 
+            paquet.append((valeur, couleur)) 
+
+    return paquet 
+
+
+def melanger_paquet(paquet):
+    """
+    Prend en paramètre un paquet de cartes sous forme de liste de tuples
+    Mélange la liste en place
+    """
+    random.shuffle(paquet)
